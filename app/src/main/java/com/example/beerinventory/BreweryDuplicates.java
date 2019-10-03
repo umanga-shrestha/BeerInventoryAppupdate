89220fa8e2c582522a5daea288a75695eb080be0
Zohar.Moreno <zmoreno08@gmail.com>
2019-10-03 18:24:08 -0500

    New class BreweryDuplicates.java added.

---
+++ app/src/main/java/com/example/beerinventory/BreweryDuplicates.java
@@ -0,0 +1,23 @@
+package com.example.beerinventory;
+
+public class BreweryDuplicates
+{
+    private String barcode;
+
+    public BreweryDuplicates (String barcode)
+    {
+        this.barcode = barcode;
+    }
+
+    public void openFileToRead ()
+    {
+
+    }
+
+    public boolean checkDuplicates ()
+    {
+        boolean flag = true;
+
+        return flag;
+    }
+}
